5efa40005355af758d9a67512a1fcff575581027
htyagi5 <harshit2310079@akgec.ac.in>
2025-05-21 23:03:26 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
-import { SocketProvider } from './context/socketProvider.jsx'
+import { SocketProvider } from './context/SocketProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
